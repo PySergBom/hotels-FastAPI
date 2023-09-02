e514effc21e0e7c77f03162509f6043e96a1c747
PySergBom <bomeevserg@gmail.com>
2023-09-03 00:36:21 +0300

    test GET method with path params, request params, POST method with params in body

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Query
+from datetime import date
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
 @app.get('/hotels')
-def get_hotels():
-    return 'Отель Бридж Резорт 5 звезд'
+def get_hotels(
+        location: str,
+        date_from: date,
+        date_to: date,
+        has_spa: bool = None,
+        stars: int = Query(None, ge=1, le=5)
+
+):
+    return date_from, date_to
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+@app.post('/bookings')
+def add_booking(booking: SBooking):
+    pass
