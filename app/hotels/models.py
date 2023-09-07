752942d0a825343d7e99d289e494149c60f17323
PySergBom <bomeevserg@gmail.com>
2023-09-07 20:53:59 +0300

    add models rooms, users, bookings

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, JSON, Boolean
+from sqlalchemy import Column, Integer, String, JSON, ForeignKey
 
 from app.database import Base
+
 
 class Hotels(Base):
     __tablename__ = 'hotels'
@@ -10,4 +11,17 @@ class Hotels(Base):
     location = Column(String, nullable=False)
     services = Column(JSON)
     rooms_quantity = Column(Integer, nullable=False)
+    image_id = Column(Integer)
+
+
+class Rooms(Base):
+    __tablename__ = 'rooms'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    hotel_id = Column(ForeignKey('hotels.id'), nullable=False)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    price = Column(Integer, nullable=False)
+    services = Column(JSON)
+    quantity = Column(Integer, nullable=False)
     image_id = Column(Integer)
--- app/migrations/env.py
+++ app/migrations/env.py
@@ -7,11 +7,13 @@ from sqlalchemy import pool
 
 from alembic import context
 
-sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
-
+from app.bookings.models import Bookings
 from app.config import settings
 from app.database import Base
-from app.hotels.models import Hotels
+from app.hotels.models import Hotels, Rooms
+from app.users.models import Users
+
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
