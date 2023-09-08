e9b55d36651aae9b517c8e1b709147a6a67afd19
PySergBom <bomeevserg@gmail.com>
2023-09-08 23:53:41 +0300

    add registration endpoint

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ from datetime import date
 from pydantic import BaseModel
 
 from app.bookings.router import router as router_bookings
+from app.users.router import router_users
 
 
 app = FastAPI()
 
+app.include_router(router_users)
 app.include_router(router_bookings)
 
 class SHotel(BaseModel):
