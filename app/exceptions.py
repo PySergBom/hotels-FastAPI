5f897e9924cec6f5a32bcf0c5cc606074459e8e2
PySergBom <bomeevserg@gmail.com>
2023-09-13 14:18:13 +0300

    Change exceptions to the classes

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,30 +1,38 @@
 from fastapi import HTTPException, status
 
-UserAlreadyExistsException = HTTPException(
-    status_code=status.HTTP_409_CONFLICT,
-    detail='User already exists'
-)
 
-IncorrectEmailOrPasswordException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail='Incorrect Email or Password'
-)
+class BookingException(HTTPException):
+    status_code = 500
+    detail = ""
 
-TokenExpiredException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail='Token expired'
-)
+    def __init__(self):
+        super().__init__(status_code=self.status_code, detail=self.detail)
 
-TokenAbsentException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail='Token absent'
-)
 
-IncorrectTokenFormatException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail='Incorrect token format'
-)
+class UserAlreadyExistsException(BookingException):
+    status_code = status.HTTP_409_CONFLICT,
+    detail = 'User already exists'
 
-UserIsNotPresentException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED
-)
+
+class IncorrectEmailOrPasswordException:
+    status_code = status.HTTP_401_UNAUTHORIZED,
+    detail = 'Incorrect Email or Password'
+
+
+class TokenExpiredException:
+    status_code = status.HTTP_401_UNAUTHORIZED,
+    detail = 'Token expired'
+
+
+class TokenAbsentException:
+    status_code = status.HTTP_401_UNAUTHORIZED,
+    detail = 'Token absent'
+
+
+class IncorrectTokenFormatException:
+    status_code = status.HTTP_401_UNAUTHORIZED,
+    detail = 'Incorrect token format'
+
+
+class UserIsNotPresentException:
+    status_code = status.HTTP_401_UNAUTHORIZED
